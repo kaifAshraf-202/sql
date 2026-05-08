f6e0f359f5f1551766fb5e774ba09dd96433c045
kaifAshraf-202 <kaifashraf027@gmail.com>
2026-05-08 22:30:47 +0530

    sql

--- sql script/SQLQuery Joins.sql
+++ sql script/SQLQuery Joins.sql
@@ -13,6 +13,48 @@ select e.ename,
 	   d.dlocation,
 	   e.deptno
 from employee as e left join dept as d on e.deptno = d.deptno
-select * from employee;
+
+
+
+
 alter table employee add mrg int;
-insert into employee (mrg) values(1842);
+insert into employee (mrg) values(1843);
+insert into employee (mrg) values(1844);
+insert into employee (mrg) values(1845);
+insert into employee (mrg) values(1844);
+insert into employee (mrg) values(1849);
+insert into employee (mrg) values(1850);
+
+delete employee where ename IS NULL;
+
+
+update employee set mrg=1843 where deptno=842;
+update employee set mrg=1844 where deptno=842;
+update employee set mrg=1849 where deptno=843;
+update employee set mrg=1842 where deptno=844;
+update employee set mrg=1850 where deptno=845;
+update employee set mrg=1845 where deptno=846;
+update employee set mrg=1823 where deptno=847;
+update employee set mrg=1833 where deptno=848;
+update employee set mrg=1853 where deptno=849;
+update employee set mrg=1853 where deptno=850;
+select * from employee;
+
+-- self join
+select e1.ename as employee , e2.ename as manager from employee e1 inner join employee e2 on e1.empno=e2.mrg;
+select 
+e1.ename as employee , 
+e2.ename as manager 
+from employee e1 
+left join employee e2 on e1.empno=e2.mrg;
+
+
+
+
+
+
+
+
+
+
+
--- sql script/SQLQuery1.sql
+++ sql script/SQLQuery1.sql
@@ -1,92 +1,23 @@
-create table customer
+-- Constraints 
+-- NOT NULL -> VALUE MUST BE THERE NULL VALUE IS NOT ALLOWED.
+-- UNIQUE -> ALL VALUES MUST BE UNIQUE ALTHOUGH NULL VALUE  IS ALLOWED 
+-- CHECK --> check the condition  [age int check (age>18);]
+-- Default --> dfault value 
+-- Primary Key -- > uniue + not null.
+-- forein key --> relation btw table maintain integrity.
+
+-- Example
+create table student
 (
-	cnum int,
-	cname varchar(10),
-	crating varchar(10)
-
+	stdID int unique,
+	stdname varchar(10) not null,
+	gender char check (gender in ('m','f','t')),
+	age int default 20 
 );
-insert into customer (cnum,cname,crating)values(01,'kaif',100);
-
-insert into customer (cnum,cname,crating)values(02,'amar',300);
-
-insert into customer (cnum,cname,crating)values(03,'kamar',200);
-
-insert into customer (cnum,cname,crating)values(04,'kasif',400);
-
-insert into customer (cnum,cname,crating)values(05,'amir',160);
-
-insert into customer (cnum,cname,crating)values(06,'adil',170);
-
-insert into customer (cnum,cname,crating)values(07,'aqib',140);
-
-insert into customer (cnum,cname,crating)values(08,'arif',900);
-
-insert into customer (cnum,cname,crating)values(09,'yusuf',800);
-
-insert into customer (cnum,cname,crating)values(010,'ismail',500);
-
-insert into customer (cnum,cname,crating)values(011,'iqbal',340);
-
-insert into customer (cnum,cname,crating)values(012,'ars',840);
-
-select * from customer;
-
-alter table customer alter column crating  int;
-
-select upper(cname) as Uppercase from customer;
-
-select sum(crating) as Sum from customer;
-
-select max(crating) as Max from customer;
-
-select min(crating) as Min from customer;
-
-select avg(crating) as Avegrage from customer;
-
-select count(*) as TotalRecaords from customer;
-
-select cname from customer 
-	group By cname, crating ;
-
-create table employee
-(
-	empno int primary key identity(01841,1),
-	ename varchar(10),
-	deptno int
-);
-insert into employee (ename,deptno)values('ars',840);
-insert into employee (ename,deptno)values('kaif',842);
-insert into employee (ename,deptno)values('asif',843);
-insert into employee (ename,deptno)values('kashif',844);
-insert into employee (ename,deptno)values('saif',845);
-insert into employee (ename,deptno)values('amir',846);
-insert into employee (ename,deptno)values('raheel',847);
-insert into employee (ename,deptno)values('masood',848);
-insert into employee (ename,deptno)values('rehan',849);
-insert into employee (ename,deptno)values('umar',850);
-
-create table dept 
-(
-	deptno int primary key identity(840,1),
-	dname varchar(10),
-	dlocation varchar(20)
-);
-insert into dept (dname,dlocation) values('accounting','mumbai');
-insert into dept (dname,dlocation) values('IT','delhi');
-insert into dept (dname,dlocation) values('sales','mumbai');
-insert into dept (dname,dlocation) values('operation','punjab');
-insert into dept (dname,dlocation) values('hr','pune');
-insert into dept (dname,dlocation) values('system','dubai');
-insert into dept (dname,dlocation) values('logistics','us');
-insert into dept (dname,dlocation) values('legal','mumbai');
-select * from employee;
-select * from dept;
-
-
-
-select e.ename,
-	   e.empno,
-	   d.dname,
-	   d.dlocation,
-	   e.deptno
-from employee as e left join dept as d on e.deptno = d.deptno
+select * from student;
+     
+Seo .
+Analytics .
+Web dev .
+Mobile .
+Excel and Google .
